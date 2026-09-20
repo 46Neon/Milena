dcf64502b16e1e7a26fa3ac0eddd8ed92cb9bb1a
46Neon <j7942281@gmail.com>
2026-09-20 16:55:45 -0700

    test: usa la sintaxis explícita de extracción de periodo

--- tests/test_language_runtime.c
+++ tests/test_language_runtime.c
@@ -93,7 +93,7 @@ int main(void) {
         "  entrada categorica \"ciudad\"\n"
         "  salida binaria \"compro\"\n"
         "  .limpiar dataset { #nulos(\"eliminar\") #duplicados(\"eliminar\") }\n"
-        "  .transformar dataset { #total(\"precio * cantidad\") #periodo(\"mes de fecha\") }\n"
+        "  .transformar dataset { #total(\"precio * cantidad\") #periodo extraer(\"mes de fecha\") }\n"
         "  .agrupar dataset { #por(\"ciudad\") #suma(\"total\") #media(\"total\") #conteo(\"total\") }\n"
         "  .unir { #derecha(\"test-language-runtime-right.csv\") #clave(\"ciudad\") }\n"
         "  .seleccionar { #columnas(\"ciudad,total_suma,periodo,region\") }\n"
