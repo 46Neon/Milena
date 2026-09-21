6a68e16edbad9967d9bf432ac630cfc340b335a3
46Neon <j7942281@gmail.com>
2026-09-21 09:21:13 -0700

    PR25: add bounded grouped streaming aggregation

--- include/ast.h
+++ include/ast.h
@@ -90,6 +90,7 @@ typedef struct ASTNode {
     size_t stream_chunk_rows;
     size_t stream_record_limit;
     size_t stream_column_limit;
+    size_t stream_group_limit;
     struct ASTNode **children;
     size_t child_count;
     size_t child_capacity;
